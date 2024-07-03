b2f05f0c2ee549708e80816941c75380f4966a30
Alexandritesu <rovick.nagutoms@example.com>
2024-07-03 17:31:12 +0800

    BACCARAT

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const pcard = document.getElementById("pcard");
+const bcard = document.getElementById("bcard");
+
+let card1p,card2p,card3p,card1b,card2b,card3b
+
+const min=1, max=9
+
+card1=Math.floor(Math.random()*max) + min;
+
